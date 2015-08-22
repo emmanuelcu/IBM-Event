c4a3f6823cb5c18a6be8ec32f6f05a1583be7863
Emmanuel <emmanuelcu@gmail.com>
2015-08-21 23:23:03 -0500

    updates in the view control

--- IBM Event/colorTabControl.swift
+++ IBM Event/colorTabControl.swift
@@ -6,4 +6,17 @@
 //  Copyright (c) 2015 Emmanuel Cuevas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class colorTabControl : UINavigationController {
+    override func viewDidLoad() {
+        UINavigationBar.appearance().barTintColor = UIColor(red: 32/255.0, green: 89/255.0, blue: 179/255.0, alpha: 1.0)
+       
+        
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
